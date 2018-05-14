0f2bd202e10e15a8500166d271f4e60b1fa64ac4
ЛатыповА <@223_9>
2018-05-14 17:10:05 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Тема: Интерфейсы.
+
+1.	Создать интерфейс IStudent: 
+●	string Name { get; set; }
+●	string FullName { get; set; }
+●	int[] Grades { get; set; }
+●	String GetName()
+●	String GetFullName() 
+●	Double GetAvgGrade() 
+Наследовать от интерфейса класс Student и реализовать его.
+*/
+
+using Interfaces.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Alfar", "Latypov");
+            string name = student.GetName(), fullName = student.GetFullName();
+            double avgGrade = student.GetAvgGrade();
+            Console.WriteLine("\n\n\t------------------------------\n\tName: {0}. \n\tFull name: {1}. \n\tAverage grade: {2}.", name, fullName, avgGrade);
+            Console.WriteLine("\t------------------------------\n\n\n");
+        }
+    }
+}
---
+++ Interfaces/classes/Student.cs
@@ -0,0 +1,47 @@
+﻿using Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.classes
+{
+    class Student : IStudent
+    {
+        private Random rand = new Random();
+        public Student(string _Name, string _FullName)
+        {
+            Name = _Name;
+            FullName = _FullName;
+            Grades = new int[rand.Next(4, 10)];
+            for (int i = 0; i < Grades.Length; i++)
+            {
+                Grades[i] = rand.Next(1, 12);
+            }
+        }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int[] Grades { get; set; }
+
+        public double GetAvgGrade()
+        {
+            int a = 0;
+            for (int i = 0; i < Grades.Length; i++)
+            {
+                a += Grades[i];
+            }
+            return (double)a / (double)Grades.Length;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
